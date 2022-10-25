e19235745a7c97fe702140a0714d975238431fa8
Leonardo Nicola <leonardonicolares@gmail.com>
2022-10-24 23:47:41 -0300

    init

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,86 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <VueApexCharts
+      :width=500
+      type="donut"
+      :options="chartOptions"
+      :series="series"
+      class="q-my-xl"
+    />
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { useExpenseStore } from '../stores/expenses'
+import VueApexCharts from 'vue3-apexcharts'
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+const expenses = useExpenseStore()
+
+const chartOptions = {
+  dataLabels: {
+    enabled: false,
+  },
+  theme: {
+    mode: 'light',
+    palette: 'palette7',
+  },
+  responsive: [
+    {
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 400,
+        },
+      },
+    },
+  ],
+  responsive: [
+    {
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 300,
+        },
+        legend: {
+          position: 'bottom'
+        },
+      },
+    },
+  ],
+  labels: ['Entertainment', 'Health', 'Essentials'],
+  plotOptions: {
+    pie: {
+      donut: {
+        labels: {
+          value: {
+            formatter: formatCurrency,
+          },
+          show: true,
+          total: {
+            show: true,
+            fontWeight: 'bold',
+            formatter: function (w) {
+              return `R$ ${w.globals.seriesTotals
+                .reduce((a, b) => {
+                  return a + b
+                }, 0)
+                .toLocaleString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`
+            },
+          },
+        },
+      },
+    },
+  },
+  yaxis: {
+    labels: {
+      formatter: formatCurrency,
+    },
+  },
+}
+const series = [expenses.entertainment.value, expenses.health.value, expenses.essentials.value]
+
+function formatCurrency(val) {
+  return `R$ ${val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`
+}
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,32 @@
-
 const routes = [
   {
     path: '/',
+    component: () => import('layouts/InitialLayout.vue'),
+    children: [{ path: '', component: () => import('pages/Login.vue') }],
+  },
+  {
+    path: '/app',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      {
+        path: 'essentials',
+        component: () => import('components/Essentials.vue'),
+      },
+      {
+        path: 'entertainment',
+        component: () => import('components/Entertainment.vue'),
+      },
+      { path: 'health', component: () => import('components/Health.vue') },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
 ]
 
 export default routes
