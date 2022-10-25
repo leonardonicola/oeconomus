4d95bfb6c418ceb8588d76d1c6634d9ee19ad972
Leonardo Nicola <leonardonicolares@gmail.com>
2022-10-24 23:45:15 -0300

    componentes de despesas por categoria

---
+++ src/components/Essentials.vue
@@ -0,0 +1,7 @@
+<template>
+  <q-page class="flex flex-center">
+   <h1>Essentials</h1>
+  </q-page>
+</template>
+
+<script setup></script>
---
+++ src/components/Health.vue
@@ -0,0 +1,8 @@
+v
+<template>
+  <q-page class="flex flex-center">
+    <h1>Health</h1>
+  </q-page>
+</template>
+
+<script setup></script>
