c674dcb92330177915e58c353fdbf1e2f4b03f4a
Leonardo Nicola <leonardonicolares@gmail.com>
2022-10-25 17:34:32 -0300

    funções de adicionar despesa e fundos

--- src/stores/expenses.js
+++ src/stores/expenses.js
@@ -1,30 +1,63 @@
-import { defineStore } from 'pinia';
+import { defineStore } from 'pinia'
+import { useCardStore } from './card'
 
 export const useExpenseStore = defineStore('expenses', {
   state: () => ({
     walletBalance: 700,
-    entertainment:{
-      value:150,
-      historic:[]
+    entertainment: {
+      historic: [
+        { name: 'Pub with boys', value: 150 },
+      ],
     },
-    health:{
-      value:550,
-      historic:[]
+    health: {
+      historic: [
+        { name: 'Laroche cream', value: 350 },
+        { name: 'Grocery', value: 200 },
+      ],
     },
-    essentials:{
-      value:50,
-      historic:[]
-    }
+    essentials: {
+      historic: [{ name: 'PSN Plus', value: 150 }],
+    },
   }),
   getters: {
     balanceWithFilter: (state) => {
-      let val = (state.walletBalance/1).toFixed(2).replace('.', ',')
-      return `R$ ${val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}`
+      let val = (state.walletBalance / 1).toFixed(2).replace('.', ',')
+      return `R$ ${val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`
+    },
+    total: (state) => (expense) => {
+      let val = state[expense].historic.reduce(
+        (acumulador, { value }) => acumulador + value,
+        0
+      )
+      return val
     },
   },
   actions: {
-
+    addExpense(expense) {
+      const card = useCardStore()
+      this[expense.type].historic.push({
+        name: expense.name,
+        value: expense.value,
+      })
+      switch(expense.origin){
+        case 'credit card':
+          card.cardBalance += expense.value
+          break;
+        case 'wallet':
+          this.walletBalance -= expense.value
+          break;
+      }
+    },
+    addFunds(funds){
+      const card = useCardStore()
+      switch(funds.origin){
+        case 'credit card payment':
+          card.cardBalance -= funds.value
+          break;
+        case 'wallet':
+          this.walletBalance += funds.value
+          break;
+      }
+    }
   },
-});
-
-
+})
