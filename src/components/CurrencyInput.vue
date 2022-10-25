940274ab0d2adaa70d86d1f4d06f7511ab3fc35b
Leonardo Nicola <leonardonicolares@gmail.com>
2022-10-25 17:30:37 -0300

    componente vue-currency-input

---
+++ src/components/CurrencyInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-input ref="inputRef" dense v-model="formattedValue" outlined lazy-rules :rules="[() => props.modelValue > 0 || 'Insert a value']"/>
+</template>
+
+<script setup>
+import { useCurrencyInput } from 'vue-currency-input'
+import { watch } from 'vue'
+
+const props = defineProps({
+  modelValue: Number,
+  options: Object,
+})
+
+const { inputRef, formattedValue, setValue } = useCurrencyInput(props.options)
+watch(
+  () => props.modelValue,
+  (value) => {
+    setValue(value)
+  }
+)
+</script>
